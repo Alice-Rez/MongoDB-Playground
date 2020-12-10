5474a34884e37eff6e3c31f6202e2df57df603d3
Alice-Rez <reznickova.alice@seznam.cz>
2020-12-10 15:29:09 +0100

    second route created

--- routes/companies.js
+++ routes/companies.js
@@ -37,4 +37,32 @@ router.get("/employers/:number", (req, res, next) => {
   });
 });
 
+router.get("/offices/:city", (req, res, next) => {
+  console.log(req.params.city);
+  var city = req.params.city;
+  MongoClient.connect(url, { useUnifiedTopology: true }, (err, db) => {
+    if (err) throw err;
+    let myDB = db.db("sample_training");
+    myDB
+      .collection("companies")
+      .find(
+        { offices: { $elemMatch: { city: city } } },
+        {
+          projection: {
+            _id: 0,
+            name: 1,
+            offices: 1,
+          },
+        }
+      )
+      .sort({ number_of_employees: 1, name: 1 })
+      // .limit(10)
+      .toArray((err, result) => {
+        if (err) throw err;
+        res.send(result);
+        db.close();
+      });
+  });
+});
+
 module.exports = router;
